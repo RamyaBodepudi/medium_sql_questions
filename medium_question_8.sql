37400aab8446a180a5681d16c3741dd1384c1732
Ramya Bodepudi <ramyabodepudi99@gmail.com>
2024-07-28 16:01:28 -0600

    medium sql question 8 added

---
+++ medium_question_8.sql
@@ -0,0 +1,4 @@
+-- 8. Show patient_id, diagnosis from admissions. Find patients admitted multiple times for the same diagnosis.
+select patient_id, diagnosis from admissions
+group by patient_id, diagnosis
+having count(*) >1;
