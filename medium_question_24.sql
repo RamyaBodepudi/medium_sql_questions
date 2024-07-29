2f0e62fba55f5974d2d7da0f4b0b513062eb5e2c
Ramya Bodepudi <ramyabodepudi99@gmail.com>
2024-07-29 11:45:10 -0600

    medium sql question 24 added

---
+++ medium_question_24.sql
@@ -0,0 +1,4 @@
+-- 24. Show patient_id, first_name, last_name from patients whose does not have any records in the admissions table. (Their patient_id does not exist in any admissions.patient_id rows.)
+select patient_id, first_name, last_name from patients
+where patient_id not in  
+(select patient_id from admissions);
