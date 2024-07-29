33f3d234df7039ee9a220d43e35bfad298950070
Ramya Bodepudi <ramyabodepudi99@gmail.com>
2024-07-29 11:26:31 -0600

    medium sql question 22 added

---
+++ medium_question_22.sql
@@ -0,0 +1,5 @@
+/* 22. display the first name, last name and number of duplicate patients based on their first name and last name.
+Ex: A patient with an identical name can be considered a duplicate. */
+select first_name, last_name, count(*) as num_of_duplicates from patients
+group by first_name, last_name
+having num_of_duplicates > 1;
